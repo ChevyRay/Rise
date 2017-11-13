d9bbb85950efe6076f18269839fcc64b483ff85d
Chevy Ray Johnston <happytrash@gmail.com>
2017-11-13 14:08:44 -0800

    removed duplicate bitmap and added generic format decoders

---
+++ Rise.Imaging/Source/FormatDecoder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Rise.Imaging
+{
+    public class FormatDecoder
+    {
+        public FormatDecoder()
+        {
+        }
+    }
+}
---
+++ Rise.Imaging/Source/ImageLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Rise.Imaging
+{
+    public class ImageDecoder
+    {
+        Dictionary<string, FormatDecoder> decoders = new Dictionary<string, FormatDecoder>(StringComparer.OrdinalIgnoreCase);
+
+        public ImageDecoder()
+        {
+            
+        }
+    }
+}
