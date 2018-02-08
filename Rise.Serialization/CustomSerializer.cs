ea99f9fb66a4795fbc8db11fe6122baa5d88ed86
Chevy Ray Johnston <happytrash@gmail.com>
2018-02-07 23:28:37 -0800

    working on serialization stuff

---
+++ Rise.Serialization/CustomSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+namespace Rise.Serialization
+{
+    public abstract class CustomSerializer
+    {
+        public void Serialize(object obj, BinaryWriter writer)
+        {
+            
+        }
+    }
+}
---
+++ Rise.Serialization/Serializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Rise.Serialization
+{
+    public class Serializer
+    {
+        public Serializer()
+        {
+            
+        }
+    }
+}
