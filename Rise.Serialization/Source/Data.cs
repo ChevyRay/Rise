6f3b71d3609020e632e92587afc9e9b00408ff10
Chevy Ray Johnston <happytrash@gmail.com>
2018-02-08 23:48:54 -0800

    more notes and split the data stuff into separate classes

---
+++ Rise.Serialization/Source/Data.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Rise.Serialization
+{
+    public class Data<T> : DataNode
+    {
+        static CustomSerializer serializer;
+
+        public T Value;
+
+        internal override void Init(DataTree tree, DataNode parent, string path)
+        {
+            base.Init(tree, parent, path);
+
+            //Hacky, but strings should default to "", not null
+            if (this is Data<string>)
+            {
+                GetType().GetField("Value").SetValue(this, string.Empty);
+            }
+        }
+
+        public override void WriteBytes(ByteWriter writer)
+        {
+            if (serializer == null)
+                serializer = CustomSerializer.Get<T>();
+            serializer.WriteBytes(Value, writer);
+        }
+
+        public override void ReadBytes(ByteReader reader)
+        {
+            if (serializer == null)
+                serializer = CustomSerializer.Get<T>();
+            Value = (T)serializer.ReadBytes(reader);
+        }
+
+        public override DataNode FindByPath(string path)
+        {
+            throw new Exception("Cannot search Data<T> by path");
+        }
+    }
+}
---
+++ Rise.Serialization/Source/SerializedData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Rise.Serialization
+{
+    public class SerializedData
+    {
+        public Type Type { get; private set; }
+        public string Path { get; private set; }
+        public byte[] Bytes { get; private set; }
+
+        internal SerializedData(Type type, string path, byte[] bytes)
+        {
+            Type = type;
+            Path = path;
+            Bytes = bytes;
+        }
+    }
+}
