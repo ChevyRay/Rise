b751877a90c5b2f99340fbf3ba326d2a9d79f3e6
Chevy Ray Johnston <happytrash@gmail.com>
2018-02-09 00:23:14 -0800

    more gui ideas

---
+++ Rise.Gui/Source/Checkbox.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Rise.Serialization;
+namespace Rise.Gui
+{
+    public partial class GuiState
+    {
+        //This is a raw version
+        public bool Checkbox(bool isChecked)
+        {
+            return isChecked;
+        }
+
+        //This version could automatically work with the data/undo stack
+        public bool Checkbox(Data<bool> isChecked)
+        {
+            bool clicked = true;
+            if (clicked)
+            {
+                isChecked.Record();
+                isChecked.Value = !isChecked.Value;
+            }
+            return isChecked.Value;
+        }
+    }
+}
