e38c3ecfaadd700cf92379cc4f68e7ead79c8cd5
Chevy Ray Johnston <happytrash@gmail.com>
2018-02-14 13:56:45 -0800

    fix incorrect heigh in atlasimage

--- Rise.Framework/Source/Atlas/AtlasImage.cs
+++ Rise.Framework/Source/Atlas/AtlasImage.cs
@@ -24,7 +24,7 @@ namespace Rise
             OffsetX = ox;
             OffsetY = oy;
             TrimWidth = tw;
-            TrimHeight = tw;
+            TrimHeight = th;
 
             if (rotate90)
             {
