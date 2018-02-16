e6e3c7bbeabff98d045c5c45b7b810fec0479528
Chevy Ray Johnston <happytrash@gmail.com>
2018-02-16 14:54:49 -0800

    remove unused junk

--- Rise.Framework/Source/Atlas/AtlasImage.cs
+++ Rise.Framework/Source/Atlas/AtlasImage.cs
@@ -1,5 +1,4 @@
-﻿using System;
-namespace Rise
+﻿namespace Rise
 {
     public class AtlasImage
     {
@@ -13,7 +12,6 @@ namespace Rise
         public int TrimHeight { get; private set; }
 
         Quad uv;
-        RectangleI trim;
 
         internal AtlasImage(Atlas atlas, ref string name, int w, int h, int ox, int oy, int tw, int th, ref Rectangle uvRect, bool rotate90)
         {
